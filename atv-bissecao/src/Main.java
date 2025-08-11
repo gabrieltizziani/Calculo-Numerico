97324789e2c47794e37ff911d0d13c7a5a9a0c45
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2025-08-11 20:25:50 -0300

    Inicio projeto

--- atv-bissecao/src/Main.java
+++ atv-bissecao/src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    //teste com set, depois colocar para o usuário interagir
+    public static double f(double x) {
+        return Math.pow(x, 3) - 9 * x + 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Digite o valor de XA (a): ");
+        double a = sc.nextDouble();
+
+        System.out.print("Digite o valor de XB (b): ");
+        double b = sc.nextDouble();
+
+        System.out.print("Digite a precisão (epsilon): ");
+        double precisao = sc.nextDouble();
+
+        if (f(a) * f(b) > 0) {
+            System.out.println("Não há garantia de raiz no intervalo (f(a) * f(b) > 0).");
+            return;
         }
+
+        double xn = 0, erro = Double.MAX_VALUE;
+        double xnAnterior;
+        int iteracao = 0;
+
+        System.out.printf("%-10s%-12s%-15s%-12s%-15s%-12s%-15s%-12s%n",
+                "Iteração", "XA", "f(XA)", "XB", "f(XB)", "XN", "f(XN)", "Erro");
+
+
+
     }
 }
